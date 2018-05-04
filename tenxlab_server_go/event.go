b11205fd5f88c495b94a65562faa5417d795c3ed
Talha Ansari <tansari@nextjump.com>
2018-05-04 14:45:25 -0400

    concurrency safe v1 + event add + godfiledb

---
+++ tenxlab_server_go/event.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"fmt"
+	"github.com/teejays/gofiledb"
+	"time"
+)
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+* E N T I T Y 										  *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+type (
+	Event struct {
+		Id            int
+		EventType     string
+		Name          string
+		DatetimeStart time.Time
+
+		// People
+		Presenters []User
+		Judges     []User
+
+		// Durations
+		EventDuration            time.Duration
+		PresentationDuration     time.Duration
+		AudienceFeedbackDuration time.Duration
+		JudgeFeedbackDuration    time.Duration
+
+		// SubEntities
+		SubEvents []SubEvent
+	}
+
+	User struct {
+		Id   int64
+		Name string
+	}
+
+	SubEvent struct {
+		// People
+		Presenter User
+		Judges    []User
+
+		// Durations
+		PresentationDuration     time.Duration
+		AudienceFeedbackDuration time.Duration
+		JudgeFeedbackDuration    time.Duration
+
+		// TimerEvents
+		TimerEvents []TimerEvent
+	}
+
+	TimerEvent struct {
+		ActivityType string
+		Title        string
+		Duration     time.Duration
+	}
+)
+
+func NewEvent(eventType, name string, datetimeStart time.Time, presenters, judges []User, eventDuration, presentationDuration, audienceFeedbackDuration, judgeFeedbackDuration time.Duration) *Event {
+
+	var e Event
+	e.EventType = eventType
+	e.Name = name
+	e.DatetimeStart = datetimeStart
+
+	e.Presenters = presenters
+	e.Judges = judges
+
+	e.EventDuration = eventDuration
+	e.PresentationDuration = presentationDuration
+	e.AudienceFeedbackDuration = audienceFeedbackDuration
+	e.JudgeFeedbackDuration = judgeFeedbackDuration
+
+	var subEvents []SubEvent
+	for _, presenter := range e.Presenters {
+		var s SubEvent
+		s.Presenter = presenter
+		s.Judges = e.Judges
+		s.PresentationDuration = e.PresentationDuration
+		s.AudienceFeedbackDuration = e.AudienceFeedbackDuration
+		s.JudgeFeedbackDuration = e.JudgeFeedbackDuration
+
+		var timerEvents []TimerEvent
+		timerEvents = append(timerEvents, TimerEvent{"Presentation", s.Presenter.Name, s.PresentationDuration})
+		timerEvents = append(timerEvents, TimerEvent{"Feedback", "Audience Feeback", s.AudienceFeedbackDuration})
+		for _, judge := range s.Judges {
+			timerEvents = append(timerEvents, TimerEvent{"Judge Feedback", judge.Name, s.JudgeFeedbackDuration})
+		}
+		s.TimerEvents = timerEvents
+
+		subEvents = append(subEvents, s)
+	}
+	e.SubEvents = subEvents
+
+	return &e
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+* M O C K  DA T A 									  *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+var mockPresenters []User = []User{
+	User{Id: 1, Name: "Abhay Sesha"},
+	User{Id: 2, Name: "Supriya Mishra"},
+	User{Id: 3, Name: "Darrell Platz"},
+}
+
+var mockJudges []User = []User{
+	User{Id: 4, Name: "Tom Fuller"},
+	User{Id: 5, Name: "Xuelan Zhang"},
+	User{Id: 6, Name: "Nayan Busa"},
+}
+var mockEvent1 *Event = NewEvent("Engineering 10x", "Engineering 10x", time.Now(), mockPresenters, mockJudges, (1 * time.Hour), (5 * time.Minute), (2 * time.Minute), (2 * time.Minute))
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+* E V E N T S										  *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+var eventsMap map[int]*Event
+
+func setupEventsMap() error {
+	db := gofiledb.GetClient()
+	exists, err := db.GetStructIfExists("storage", "tenxlab_events_map", &eventsMap)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		eventsMap = make(map[int]*Event)
+	}
+	return nil
+}
+
+func addEvent(e Event) error {
+	// validate event
+	err := validateEvent(e)
+	if err != nil {
+		return err
+	}
+	// we should give this an event an id
+	// get the max id of the existing events and increment
+	var maxId int
+	for _, e := range eventsMap {
+		if e.Id > maxId {
+			maxId = e.Id
+		}
+	}
+	// new id should be 1 + existing max id
+	e.Id = maxId + 1
+	eventsMap[e.Id] = &e
+
+	// save the new events map in the db
+	db := gofiledb.GetClient()
+	err = db.SetStruct("storage", "tenxlab_events_map", eventsMap)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func validateEvent(e Event) error {
+	if e.EventType == "" {
+		return fmt.Errorf("invalid event type: %s", e.EventType)
+	}
+
+	if len(e.Presenters) < 1 {
+		return fmt.Errorf("no presenters provided")
+	}
+
+	if len(e.Judges) < 1 {
+		return fmt.Errorf("no presenters provided")
+	}
+
+	if e.PresentationDuration < 1000 {
+		return fmt.Errorf("invalid presentation duration: %d", e.PresentationDuration)
+	}
+
+	return nil
+
+}
+
+func getEvents() []*Event {
+	var events []*Event
+	for _, e := range eventsMap {
+		events = append(events, e)
+	}
+	return events
+}
+
+func getEventById(eventId int) (*Event, error) {
+	e, exists := eventsMap[eventId]
+	if !exists {
+		return nil, fmt.Errorf("No event exists for event id %d", eventId)
+	}
+	return e, nil
+}
--- tenxlab_server_go/main.go
+++ tenxlab_server_go/main.go
@@ -3,26 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	//"golang.org/x/net/websocket"
-	//"io"
-
-	"net"
-	"net/http"
 
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
+	"github.com/teejays/gofiledb"
+	"net"
+	"net/http"
+	"sync"
+	"time"
 )
 
 // This example demonstrates a trivial echo server.
 func main() {
+	// set the app
+	gofiledb.InitClient("data")
+	err := setupEventsMap()
+	if err != nil {
+		panic(err)
+	}
+
+	// set the webserver
 	http.HandleFunc("/ws", handlerWs)
 
-	http.HandleFunc("/events", handlerEvents)
+	http.HandleFunc("/event", handlerEvents)
 	http.HandleFunc("/screen/connect", handlerScreenConnect)
 	http.HandleFunc("/screen/control", handlerScreenControl)
 
 	fmt.Println("Listening for requests..")
-	err := http.ListenAndServe(":3000", nil)
+	err = http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
@@ -31,11 +39,22 @@ func main() {
 /* H A N D L E R S */
 
 func handlerEvents(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
+	switch r.Method {
+	case "GET":
+		handlerGetEvents(w, r)
+		break
+	case "POST":
+		handlerPostEvent(w, r)
+		break
+	default:
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(fmt.Sprintf("No %s endpoint implement for the route", r.Method)))
 		return
 	}
+
+}
+
+func handlerGetEvents(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Request received for Events..")
 
 	events := getEvents()
@@ -43,10 +62,46 @@ func handlerEvents(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
-
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(bytes)
+}
+
+type AddEventParams struct {
+	EventType                string
+	Name                     string
+	DatetimeStart            time.Time
+	Presenters               []User
+	Judges                   []User
+	EventDuration            time.Duration
+	PresentationDuration     time.Duration
+	AudienceFeedbackDuration time.Duration
+	JudgeFeedbackDuration    time.Duration
+}
+
+func handlerPostEvent(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request received for adding events..")
+
+	// get the params
+	decoder := json.NewDecoder(r.Body)
+	var p AddEventParams
+
+	err := decoder.Decode(&p)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	event := NewEvent(p.EventType, p.Name, p.DatetimeStart, p.Presenters, p.Judges, p.EventDuration, p.PresentationDuration, p.AudienceFeedbackDuration, p.JudgeFeedbackDuration)
+	err = addEvent(*event)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write([]byte("Submitted"))
 }
 
 type ReponseStruct struct {
@@ -83,7 +138,9 @@ func handlerScreenConnect(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// check if code is valid
+	screenCodeConnectionMapLock.RLock()
 	_, exists := screenCodeConnectionMap[params.Code]
+	screenCodeConnectionMapLock.RUnlock()
 	if !exists {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Invalid code: no screen found for the given code."))
@@ -103,18 +160,20 @@ func handlerScreenConnect(w http.ResponseWriter, r *http.Request) {
 	exists = true
 	for exists { // get a token until we have a new unused one
 		token = getUniqueAppToken()
+		appTokenScreenCodeMapLock.RLock()
 		_, exists = appTokenScreenCodeMap[token]
+		appTokenScreenCodeMapLock.RUnlock()
 	}
 	fmt.Println("AppToken Generated: " + token)
+	appTokenScreenCodeMapLock.Lock()
 	appTokenScreenCodeMap[token] = params.Code
+	appTokenScreenCodeMapLock.Unlock()
 
 	// send the event data to the screen
-	go func(code string, event *Event) {
-		err = sendWsDataByCode(params.Code, WsMessage{"Event", event})
-		if err != nil {
-			fmt.Println(err)
-		}
-	}(params.Code, event)
+	err = sendWsDataByCode(params.Code, WsMessage{"Event", event})
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	w.Write([]byte(token))
 
@@ -151,7 +210,9 @@ func handlerScreenControl(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// check what screen the app token points to
+	appTokenScreenCodeMapLock.RLock()
 	code, exists := appTokenScreenCodeMap[params.AppToken]
+	appTokenScreenCodeMapLock.RUnlock()
 	if !exists {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Invalid token: seems like the app is not linked to any screen."))
@@ -184,7 +245,10 @@ func handlerScreenControl(w http.ResponseWriter, r *http.Request) {
 }
 
 var screenCodeConnectionMap map[string]net.Conn
+var screenCodeConnectionMapLock sync.RWMutex
+
 var appTokenScreenCodeMap map[string]string
+var appTokenScreenCodeMapLock sync.RWMutex
 
 func init() {
 	screenCodeConnectionMap = make(map[string]net.Conn)
@@ -218,12 +282,19 @@ func handlerWs(w http.ResponseWriter, r *http.Request) {
 
 		// get code for the web client and store it in the map
 		code := getUniqueScreenCode()
+		appTokenScreenCodeMapLock.RLock()
 		if _, exists := screenCodeConnectionMap[code]; exists {
+			appTokenScreenCodeMapLock.RUnlock()
 			fmt.Println("the code is already in use")
 			return
 		}
+		appTokenScreenCodeMapLock.RUnlock()
 
+		// save the screen code and the connection
+		appTokenScreenCodeMapLock.Lock()
 		screenCodeConnectionMap[code] = conn
+		appTokenScreenCodeMapLock.Unlock()
+
 		fmt.Printf("Opened WS: %s\n", code)
 
 		// send the unique code to web client
@@ -239,18 +310,23 @@ func handlerWs(w http.ResponseWriter, r *http.Request) {
 				removeWsConnectionByCode(code)
 				break
 			}
+			// do something with the message
 			fmt.Printf("WS Message Received: %v, %v\n", msg, op)
 		}
 	}()
 }
 
 func removeWsConnectionByCode(code string) {
+	screenCodeConnectionMapLock.Lock()
 	delete(screenCodeConnectionMap, code)
+	screenCodeConnectionMapLock.Unlock()
 }
 
 func sendWsDataByCode(code string, data interface{}) error {
 	// get the connection
+	screenCodeConnectionMapLock.RLock()
 	conn, exists := screenCodeConnectionMap[code]
+	screenCodeConnectionMapLock.RUnlock()
 	if !exists {
 		return fmt.Errorf("No connection record for code %s exists", code)
 	}
